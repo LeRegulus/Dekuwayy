1bca1be662d732bb7c163418962e95ef8c6668cb
ReglusAlou <alassaneseck94@gmail.com>
2021-08-14 02:14:36 +0000

    Smtp3

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,7 +46,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('samadekuway@gmail.com', 'Dekuwayy Bot'))
+                    ->from(new Address('alassaneseck94@gmail.com', 'Dekuwayy Bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
